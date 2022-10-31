c900834433be64ff8f9741ba1238802e0c8b2421
dasya27 <113109207+dasya27@users.noreply.github.com>
2022-10-31 21:23:32 +0300

    Update main.js

--- web5/main.js
+++ web5/main.js
@@ -1,9 +1,33 @@
+
+// объвляем 2 переменные, то, что человек вводит с клавиатуры
+// inputNum - количество товаров
+// inputPrice - количество товаров
+
+//querySelector() - помогает получить элемент, 
+//который подходит селектор, указанный в скобках
+
+
 let inputNum = document.querySelector('.box_number');
 let inputPrice = document.querySelector('.box_price');
 
-let button = document.querySelector('.box_btn');
+ //input.value передает нам то число, которое ввел пользователь 
 
-button.onclick = function() {
-    console.log(inputNum.value * inputPrice.value);
-    document.getElementById("box_ans_out").innerHTML = inputNum.value * inputPrice.value;
+function answer() {
+    if(inputNum.value == "" || inputPrice.value == "")
+    {
+        alert("Вы ввели не все данные")
+    }
+    else
+    {
+        if(inputNum.value <0 || inputPrice.value < 0)
+        {
+            alert("Нельзя вводить отрицательные значения:)")
+        }
+        else 
+        {
+            document.querySelector('.box_ans_out').innerHTML = inputNum.value * inputPrice.value;
+        }
+    }
 }
+
+//свойство InnerHTML помогает изменить содержимое элемента

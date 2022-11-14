09323a545e35bc797db1bd18baab1691f900c83f
dasya27 <113109207+dasya27@users.noreply.github.com>
2022-11-14 23:20:51 +0300

    Update main.js

--- web5/main.js
+++ web5/main.js
@@ -1,33 +1,44 @@
 
-// объвляем 2 переменные, то, что человек вводит с клавиатуры
-// inputNum - количество товаров
-// inputPrice - количество товаров
+document.addEventListener('DOMContentLoaded', function () {
+    // объвляем 2 переменные, то, что человек вводит с клавиатуры
+    // inputNum - количество товаров
+    // inputPrice - цена товара
 
-//querySelector() - помогает получить элемент, 
-//который подходит селектор, указанный в скобках
+    //querySelector() - помогает получить элемент, 
+    //который подходит селектор, указанный в скобках
 
 
-let inputNum = document.querySelector('.box_number');
-let inputPrice = document.querySelector('.box_price');
+    let ans = document.querySelector('.box_btn');
 
- //input.value передает нам то число, которое ввел пользователь 
+    //обработчики событий
 
-function answer() {
-    if(inputNum.value == "" || inputPrice.value == "")
-    {
-        alert("Вы ввели не все данные")
-    }
-    else
-    {
-        if(inputNum.value <0 || inputPrice.value < 0)
+    ans.addEventListener("click", function(){
+
+        let inputNum = document.querySelector('.box_number');  //количество товара
+        let inputPrice = document.querySelector('.box_price');  //цена товара
+
+        if(inputNum.value == "" || inputPrice.value == "")
         {
-            alert("Нельзя вводить отрицательные значения:)")
+            alert("Вы ввели не все данные")
         }
-        else 
+        else
         {
-            document.querySelector('.box_ans_out').innerHTML = inputNum.value * inputPrice.value;
+            if(inputNum.value <0 || inputPrice.value < 0)
+            {
+                alert("Нельзя вводить отрицательные значения:)")
+            }
+            else 
+                if(isNaN(inputNum.value) || isNaN(inputPrice.value))
+                {
+                    alert("Только числа, пожалуйста");
+                }
+                else
+                {
+                    document.querySelector('.box_ans_out').innerHTML = inputNum.value * inputPrice.value;
+                    console.log(inputNum.value * inputPrice.value);
+                }
         }
-    }
-}
+    });
 
-//свойство InnerHTML помогает изменить содержимое элемента
+    //свойство InnerHTML помогает изменить содержимое элемента
+});

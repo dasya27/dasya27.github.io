e296a3c718db488c3894a4213904aa36b242bfc3
dasya27 <113109207+dasya27@users.noreply.github.com>
2022-11-14 23:24:40 +0300

    Update main.js

--- web7/main.js
+++ web7/main.js
@@ -1,6 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
 $('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4
+    infinite: true,          //бесконечная прокрутка
+    dots: true,              //точки
+    slidesToShow: 4,         //слайды для показа
+    slidesToScroll: 4,       //слайды для прокуртки
+
+    responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        }
+    ]
 });
+})
 

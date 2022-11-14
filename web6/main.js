ed5fa4e7672585a0e22ed190148b0f2e11cfbe77
dasya27 <113109207+dasya27@users.noreply.github.com>
2022-11-14 23:23:00 +0300

    Update main.js

--- web6/main.js
+++ web6/main.js
@@ -1,114 +1,140 @@
+document.addEventListener('DOMContentLoaded', function () {
+        let summ = 0;
+    let bag = 0;
+    let qual = 0;
 
-let summ = 0;
+    fl = 0;
 
-let course;
+    //переменные, которые проверяют, все ли пользователь отметил
 
-let bag = 0;
-let qual = 0;
+    //Дубай и Португалия - 100 000
+    //США и Испания - 80 000
+    //Германия - 30 000
 
-fl = 0;
+    //Багаж 5 000
+    //Бизнес класс - 7 000
 
-//переменные, которые проверяют, все ли пользователь отметил
+    //при изменении страны вызывается эта функция и меняется значение суммы
 
-//Дубай и Португалия - 100 000
-//США и Испания - 80 000
-//Германия - 30 000
+    //обрабатываем именно сумму билета
 
-//Багаж 5 000
-//Бизнес класс - 7 000
-
-//при изменении страны вызывается эта функция и меняется значение суммы
-
-//обрабатываем именно сумму билета
-function getCourse()
-{
-    course = document.querySelector(".box_select");
-
-    document.querySelector(".box_ans").style.display = "none";
-    fl=1;
-
-    console.log(course.value);
-    if(course.value == 1 || course.value == 5) //Дубай и Португалия
-    {
-        summ = 100000;
-
-        //доступен только бизнес класс
-        qual = 7000;
-        document.querySelector(".only_b").style.display = "flex";
-        document.querySelector(".class_wrapper").style.display = "none";
-
-        //доступно только с ручной кладью
-        bag = 0;
-        document.querySelector(".only_hand").style.display = "flex";
-        document.querySelector(".bag_wrapper").style.display = "none";
-    }
-    if(course.value == 2) //Германия
-    {
-        summ = 30000;
-
-        document.querySelector(".only_b").style.display = "none";
-        document.querySelector(".class_wrapper").style.display = "flex";
-
-        document.querySelector(".only_hand").style.display = "none";
-        document.querySelector(".bag_wrapper").style.display = "flex";
-    }
-    if(course.value == 3 || course.value == 4 ) //Испания и США
-    {
-        summ = 80000;
-
-        //доступен только бизнес
-        qual = 7000;
-
-        document.querySelector(".only_b").style.display = "flex";
-        document.querySelector(".class_wrapper").style.display = "none";
-
-        document.querySelector(".only_hand").style.display = "none";
-        document.querySelector(".bag_wrapper").style.display = "flex";
-    }
-}
-
-//обрабатываем качество полета
-//по умолчанию эконом класс
+    let country = document.querySelector('.country');  //получили страну
 
 
-function getBag()
-{
-    if(document.querySelector(".bag-check-b").checked)
-        bag = 5000;
-    else
-        bag = 0;
-}
+    //обработчкик события ( страна изменяется )
 
-function getQual()
-{
-    if(document.querySelector(".class-check-b").checked)
-        qual = 7000;
-    else
-        qual = 0;
-}
+    country.addEventListener('change', function() {
 
+        fl = 1;
+        //убираем поле ответа
+        document.querySelector(".box_ans").style.display = "none";
 
-
-function buy()
-{
-    let n = document.querySelector(".form-control").value;
-    console.log(n);
-    
-    if(n !="" && fl == 1)
-    {
-        if(n > 0)
+        if(country.value == 1) //Дубай
         {
-            document.querySelector(".box_ans").style.display = "block";
+            summ = 100000;
 
-            summ = (summ + bag + qual)*n;
-            document.querySelector(".answer").innerHTML = "Цена вашей покупки: " + summ;
-            summ = summ/n - bag - qual;
+            //доступен только бизнес класс
+            qual = 7000;
+            document.querySelector(".only_b").style.display = "flex";
+            document.querySelector(".class_wrapper").style.display = "none";
+
+            //доступно только с ручной кладью
+            bag = 0;
+            document.querySelector(".only_hand").style.display = "flex";
+            document.querySelector(".bag_wrapper").style.display = "none";
+        }
+
+        if(country.value == 5) // Португалия
+        {
+            summ = 100000;
+
+            document.querySelector(".only_b").style.display = "none";
+            document.querySelector(".class_wrapper").style.display = "flex";
+
+            //доступно только с ручной кладью
+            bag = 0;
+            document.querySelector(".only_hand").style.display = "flex";
+            document.querySelector(".bag_wrapper").style.display = "none";
+        }
+        
+        if(country.value == 2) //Германия
+        {
+            summ = 30000;
+
+            document.querySelector(".only_b").style.display = "none";
+            document.querySelector(".class_wrapper").style.display = "flex";
+
+            document.querySelector(".only_hand").style.display = "none";
+            document.querySelector(".bag_wrapper").style.display = "flex";
+        }
+        if(country.value == 3 || country.value == 4 ) //Испания и США
+        {
+            summ = 80000;
+
+            //доступен только бизнес
+            qual = 7000;
+
+            document.querySelector(".only_b").style.display = "flex";
+            document.querySelector(".class_wrapper").style.display = "none";
+
+            document.querySelector(".only_hand").style.display = "none";
+            document.querySelector(".bag_wrapper").style.display = "flex";
+        }
+    })
+
+
+
+
+    let buy = document.querySelector('.btn');  //получаем кнопку
+
+    buy.addEventListener('click', function () {
+        
+        let n = document.querySelector(".form-control").value;
+
+        //проверяем качество полета
+        let quals = document.querySelector(".class-check-b");
+
+        if(quals.checked)
+                {
+                    qual = 7000;
+                }
+            else
+                {
+                    qual = 0;
+                }
+        
+        //обрабатываем багаж
+        let bags = document.querySelector(".bag-check-b");
+
+        if(bags.checked)
+            bag = 5000;
+        else
+            bag = 0;
+        
+
+        if(fl==1)
+        {
+            if(n=="")
+                alert("Вы ввели не все данные");
+
+            else 
+            if(isNaN(n))
+                alert("Пожалуйста, только числа");
+            else
+                if(n > 0)
+                {
+                    document.querySelector(".box_ans").style.display = "block";
+
+                    summ = (summ + bag + qual)*n;
+                    document.querySelector(".answer").innerHTML = "Цена вашей покупки: " + summ;
+                    summ = summ/n - bag - qual;
+                }
+                else
+                    alert("Количество товаров не может быть отрицательным");
         }
         else
-        alert("Количество товаров не может быть отрицательным");
-    }
-    else
-    {
-        alert("Вы ввели не все данные");
-    }
-}
+            alert("Вы ввели не все данные");
+                
+    })
+
+});
